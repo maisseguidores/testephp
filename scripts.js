6e54bee8e4c033b2812f6e810f8f4eded7287996
maisseguidores <123846631+maisseguidores@users.noreply.github.com>
2023-02-06 01:06:17 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+//evento javascript
+function clicar(){  //<- onclick
+    
+    
+    
+    
+    
+    //variavel javascript
+    var senha=document.getElementById("senha"); //<- id
+    
+    
+    
+    
+    
+    //condiçaõ javaccript
+    if(senha.type=="password"){
+        senha.type="text";
+    }else{
+senha.type="password";
+    }
+}
